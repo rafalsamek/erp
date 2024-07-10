df5987032a5c584ae61dc44c46805d47f9b94a55
Rafal Samek <rsamek@datto.com>
2024-07-10 22:41:47 +0200

    Flyway setup

--- backend/src/main/java/com/smartvizz/erp/backend/services/DocumentService.java
+++ backend/src/main/java/com/smartvizz/erp/backend/services/DocumentService.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 @Service
 public class DocumentService {
--- backend/src/main/resources/dev/db/migration/V2__Insert_into_documents_table.sql
+++ backend/src/main/resources/dev/db/migration/V2__Insert_into_documents_table.sql
@@ -1,6 +1,3 @@
 INSERT INTO documents (title, description) VALUES ('Wniosek o dane z PESEL', 'wniosek o dane pacjenta nieubezpieczonego');
 INSERT INTO documents (title, description) VALUES ('wniosek o kuratora', 'wniosek o kuratora dla pacjenta nieubezpieczonego po dwóch zobowiązaniach i dwóch próbach doręczenia ');
 INSERT INTO documents (title, description) VALUES ('Wniosek o doręczenie korespondencji', 'wniosek o doręczenie korespondencji, kolejna próba doręczenia, Krowodrza');
-
-
-
