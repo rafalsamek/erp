759eb5f547a5fdc3a276d449eaf755f59e051741
eslota <ewa4852@gmail.com>
2024-07-18 23:42:21 +0200

    frontend: header and top-menu components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {TopMenuComponent} from "./top-menu/top-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, TopMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
