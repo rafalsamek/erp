de7fc4b7f0ff5ac61b746f98ea383aae41d299f7
eslota <ewa4852@gmail.com>
2024-07-18 23:47:59 +0200

    frontend: container and footer components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {TopMenuComponent} from "./top-menu/top-menu.component";
+import {ContainerComponent} from "./container/container.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TopMenuComponent],
+  imports: [RouterOutlet, HeaderComponent, TopMenuComponent, ContainerComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
