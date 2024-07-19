c18472c5dee0be25a9a0afab90855a1c3c75291d
eslota <ewa4852@gmail.com>
2024-07-19 16:40:23 +0200

    frontend: crud-table component

--- frontend/src/app/container/documents/documents.component.ts
+++ frontend/src/app/container/documents/documents.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {CrudHeaderComponent} from "./crud-header/crud-header.component";
+import {CrudTableComponent} from "./crud-table/crud-table.component";
 
 @Component({
   selector: 'app-documents',
   standalone: true,
   imports: [
-    CrudHeaderComponent
+    CrudHeaderComponent,
+    CrudTableComponent
   ],
   templateUrl: './documents.component.html',
   styleUrl: './documents.component.css'
