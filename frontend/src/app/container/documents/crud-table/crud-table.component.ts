9d8c63e64338e7ea2980c87461ea17a16c7f206e
eslota <ewa4852@gmail.com>
2024-07-19 21:34:19 +0200

    frontend: crud-table component

--- frontend/src/app/container/documents/crud-table/crud-table.component.ts
+++ frontend/src/app/container/documents/crud-table/crud-table.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {DocumentEntity} from "../../../models";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-crud-table',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './crud-table.component.html',
   styleUrl: './crud-table.component.css'
 })
 export class CrudTableComponent {
-
+  @Input() documentsList: DocumentEntity[] = [];
 }
--- frontend/src/app/container/documents/documents.component.ts
+++ frontend/src/app/container/documents/documents.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {CrudHeaderComponent} from "./crud-header/crud-header.component";
 import {CrudTableComponent} from "./crud-table/crud-table.component";
 import {PaginationComponent} from "./pagination/pagination.component";
+import {DocumentEntity} from "../../models";
 
 @Component({
   selector: 'app-documents',
@@ -15,5 +16,36 @@ import {PaginationComponent} from "./pagination/pagination.component";
   styleUrl: './documents.component.css'
 })
 export class DocumentsComponent {
+  ngOnInit(): void {
+    this.fetchDocuments();
+  }
 
+  documentsList: DocumentEntity[] = [];
+
+  fetchDocuments() {
+    this.documentsList = [
+      {
+        id: 1,
+        title: 'Document 1',
+        description: 'Description for document 1',
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        id: 2,
+        title: 'Document 2',
+        description: 'Description for document 2',
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        id: 3,
+        title: 'Document 3',
+        description: 'Description for document 3',
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      }
+    ];
+    return this.documentsList;
+  }
 }
