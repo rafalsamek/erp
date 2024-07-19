9742537f17fa434784922a353fdbb92d9dbb33b4
eslota <ewa4852@gmail.com>
2024-07-19 16:37:46 +0200

    frontend: crud-header component

---
+++ frontend/src/app/container/documents/crud-header/crud-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-header.component.html',
+  styleUrl: './crud-header.component.css'
+})
+export class CrudHeaderComponent {
+
+}
---
+++ frontend/src/app/container/documents/crud-table/crud-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-table.component.html',
+  styleUrl: './crud-table.component.css'
+})
+export class CrudTableComponent {
+
+}
