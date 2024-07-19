043797ffdd9a26f871ede37d143beaea3b89a075
eslota <ewa4852@gmail.com>
2024-07-19 16:23:08 +0200

    frontend: documents component

--- frontend/src/app/container/container.component.ts
+++ frontend/src/app/container/container.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {DocumentsComponent} from "./documents/documents.component";
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [],
+  imports: [
+    DocumentsComponent
+  ],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css'
 })
