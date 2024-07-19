cbeb1deaadb841cc112d023f798e23fb5183cc62
eslota <ewa4852@gmail.com>
2024-07-19 16:43:33 +0200

    frontend: pagination component

--- frontend/src/app/container/documents/documents.component.ts
+++ frontend/src/app/container/documents/documents.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {CrudHeaderComponent} from "./crud-header/crud-header.component";
 import {CrudTableComponent} from "./crud-table/crud-table.component";
+import {PaginationComponent} from "./pagination/pagination.component";
 
 @Component({
   selector: 'app-documents',
   standalone: true,
   imports: [
     CrudHeaderComponent,
-    CrudTableComponent
+    CrudTableComponent,
+    PaginationComponent
   ],
   templateUrl: './documents.component.html',
   styleUrl: './documents.component.css'
