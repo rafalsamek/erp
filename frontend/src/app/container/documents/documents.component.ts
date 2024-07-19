f0e8fb9ba280e0f6ce95f219642a779df7ce8f6c
eslota <ewa4852@gmail.com>
2024-07-19 16:37:19 +0200

    frontend: crud-header component

--- frontend/src/app/container/documents/documents.component.ts
+++ frontend/src/app/container/documents/documents.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {CrudHeaderComponent} from "./crud-header/crud-header.component";
 
 @Component({
   selector: 'app-documents',
   standalone: true,
-  imports: [],
+  imports: [
+    CrudHeaderComponent
+  ],
   templateUrl: './documents.component.html',
   styleUrl: './documents.component.css'
 })
