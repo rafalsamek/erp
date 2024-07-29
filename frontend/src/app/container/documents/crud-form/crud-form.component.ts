b76d8f842e41a4b1c7220ae85bc757a60dbd5601
eslota <ewa4852@gmail.com>
2024-07-30 01:53:55 +0200

    frontend: documents - form validation

--- frontend/src/app/container/documents/crud-form/crud-form.component.ts
+++ frontend/src/app/container/documents/crud-form/crud-form.component.ts
@@ -6,16 +6,14 @@ import {
   OnInit,
   HostListener,
 } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import {CommonModule, NgIf} from '@angular/common';
+import {FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {DocumentEntity} from "../document-entity.model";
 
 @Component({
   selector: 'documents-crud-form',
   standalone: true,
-  imports: [
-    FormsModule,
-    NgIf
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './crud-form.component.html',
   styleUrl: './crud-form.component.css'
 })
@@ -23,10 +21,30 @@ import {CommonModule, NgIf} from '@angular/common';
 export class CrudFormComponent implements OnInit {
   @Input() showModal = false;
   @Input() mode: 'add' | 'edit' | 'view' = 'view';
-  @Input() document: any = {};
+  @Input() document: DocumentEntity | null = null;
   @Output() close = new EventEmitter<void>();
-  @Output() save = new EventEmitter<any>();
+  @Output() save = new EventEmitter<DocumentEntity>();
 
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.required],
+      description: [''],
+    });
+  }
+
+  ngOnInit() {
+    if (this.document) {
+      this.form.setValue({
+        title: this.document.title,
+        description: this.document.description || '',
+      });
+    }
+    if (this.mode === 'view') {
+      this.form.disable();
+    }
+  }
 
   @HostListener('document:keydown.escape', ['$event'])
   handleEscape(event: KeyboardEvent) {
@@ -39,8 +57,16 @@ export class CrudFormComponent implements OnInit {
   }
 
   onSubmit() {
-    this.save.emit(this.document);
-    this.closeModal();
+      if (this.form.valid) {
+        const formValue = this.form.value;
+        const documentToSave = {
+          ...formValue,
+        };
+        this.save.emit(documentToSave);
+        this.closeModal();
+      } else {
+        this.form.markAllAsTouched();
+      }
   }
 
   getTitle(): string {
@@ -51,9 +77,6 @@ export class CrudFormComponent implements OnInit {
     } else {
       return 'View Document';
     }
-  }
-
-  ngOnInit(): void {
   }
 }
 
