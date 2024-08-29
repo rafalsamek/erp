43d87c7ef8436af7aa824f349515e3e672c54696
eslota <ewa4852@gmail.com>
2024-08-29 02:09:59 +0200

    [ERP-58] backend: relation documents -> categories - DocumentSpecifications updated

--- backend/src/main/java/com/smartvizz/erp/backend/data/specifications/DocumentSpecifications.java
+++ backend/src/main/java/com/smartvizz/erp/backend/data/specifications/DocumentSpecifications.java
@@ -1,6 +1,7 @@
 package com.smartvizz.erp.backend.data.specifications;
 
 import com.smartvizz.erp.backend.data.entities.DocumentEntity;
+import com.smartvizz.erp.backend.data.entities.CategoryEntity; // Ensure the import
 import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
@@ -16,11 +17,12 @@ public class DocumentSpecifications {
     public static Specification<DocumentEntity> searchDocument(String searchBy) {
         return (root, query, builder) -> {
             if (searchBy == null || searchBy.isEmpty()) {
-                // Return an empty predicate list if searchBy is null or empty
                 return builder.conjunction();
             }
+
             List<Predicate> predicateList = new ArrayList<>();
 
+            // Existing search criteria
             predicateList.add(
                     builder.equal(builder.toString(root.get("id")), searchBy)
             );
@@ -36,15 +38,26 @@ public class DocumentSpecifications {
             predicateList.add(
                     builder.like(builder.toString(root.get("updatedAt")), "%" + searchBy + "%")
             );
+
+            // Join with TemplateEntity
             Join<?, ?> templateJoin = root.join("template");
             predicateList.add(
-                    builder.like(builder.lower(templateJoin.get("name")), "%" + searchBy.toLowerCase() + "%")
+                    builder.like(builder.lower(templateJoin.get("title")), "%" + searchBy.toLowerCase() + "%")
             );
             predicateList.add(
                     builder.like(builder.lower(templateJoin.get("description")), "%" + searchBy.toLowerCase() + "%")
             );
 
-            return builder.or(predicateList.toArray(new Predicate[]{}));
+            // Join with CategoryEntity
+            Join<DocumentEntity, CategoryEntity> categoryJoin = root.join("categories");
+            predicateList.add(
+                    builder.like(builder.lower(categoryJoin.get("name")), "%" + searchBy.toLowerCase() + "%")
+            );
+            predicateList.add(
+                    builder.like(builder.lower(categoryJoin.get("description")), "%" + searchBy.toLowerCase() + "%")
+            );
+
+            return builder.or(predicateList.toArray(new Predicate[0]));
         };
     }
 
