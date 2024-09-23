2835df522e1a74d5ae326bf0924056d22bcbbb33
eslota <ewa4852@gmail.com>
2024-09-24 00:25:51 +0200

    prod: certbot optimization

--- .docker/certbot/init-certbot.sh
+++ .docker/certbot/init-certbot.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-certbot certonly --webroot --webroot-path=/var/www/certbot \
-  --email ewa4852@gmail.com --agree-tos --no-eff-email \
-  -d erp.smartvizz.com
+
+# Generate the certificate if it doesn't exist
+if [ ! -f "/etc/letsencrypt/live/erp.smartvizz.com/fullchain.pem" ]; then
+  certbot certonly --webroot --webroot-path=/var/www/certbot \
+    --email ewa4852@gmail.com --agree-tos --no-eff-email \
+    -d erp.smartvizz.com
+fi
+
+# Start a loop to renew the certificate every 12 hours
+while :; do
+  certbot renew
+  sleep 12h
+done
